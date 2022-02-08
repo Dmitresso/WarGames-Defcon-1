16f3bdaa8c791f0c5138c0bbc5c412ddb815961a
Dmitresso <dmitresso@gmail.com>
2022-02-08 21:28:36 +0700

    Added controllers

---
+++ Assets/WarGames-Defcon-1/Code/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿namespace WarGames_Defcon_1.Code.Scripts.Controller {
+    public class PlayerController : RigidbodyController {
+        #region Builtin Methods
+
+        public override void Start() {
+            base.Start();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/WarGames-Defcon-1/Code/Scripts/Controller/RigidbodyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+namespace WarGames_Defcon_1.Code.Scripts.Controller {
+    [RequireComponent(typeof(Rigidbody))]
+    public class RigidbodyController : MonoBehaviour {
+        #region Variables
+        protected Rigidbody rb;
+        #endregion
+
+
+        
+        #region Builtin Methods
+
+        public virtual void Start() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate() {
+            if (rb) HandlePhysics();
+        }
+
+        #endregion
+
+        
+
+        #region Custom Methods
+        
+        protected virtual void HandlePhysics() {
+            
+        }
+
+        #endregion
+    }
+}
